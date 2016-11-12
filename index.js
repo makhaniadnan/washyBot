c9b4e7eec9e8739f1261102558db746c01d522d8
Adnan Makhani <makhaniadnan@gmail.com>
2016-11-12 07:02:45 +0100

    changing the port

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.post('/new-message', function(req, res) {
 });
 
 // Finally, start our server
-app.listen(3000, function() {
-  console.log('Telegram app listening on port 3000!');
+app.listen(8443, function() {
+  console.log('Telegram app listening on port 8443!');
 });
